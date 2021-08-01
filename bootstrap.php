664ef7e3e74b80fe5b70e6754e244c328fca9e7d
Povilas Brilius <pbrilius@gmail.com>
2021-08-01 20:58:56 +0300

    USA impeachment - environmental variables working coherently - and smoothly, by the way.

--- bootstrap.php
+++ bootstrap.php
@@ -4,18 +4,20 @@ use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
 require_once "vendor/autoload.php";
+require_once __DIR__ . '/config/eniac.php';
 
 $isDevMode = true;
 $proxyDir = null;
 $cache = null;
 $useSimpleAnnotationReader = false;
-$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode, $isDevMode, $proxyDir, $cache);
+$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"),
+ $isDevMode, $isDevMode, $proxyDir, $cache);
 
 $dbParams = array(
-    'driver'   => 'pdo_mysql',
-    'user'     => 'getloan_doctrine',
-    'password' => 'èóíßù5íWêïË4458é',
-    'dbname'   => 'getloan_com',
+    'driver'   => $eniac->get('database.driver'),
+    'user'     => $eniac->get('database.username'),
+    'password' => $eniac->get('database.password'),
+    'dbname'   => $eniac->get('database.database'),
 );
 
 // obtaining the entity manager
--- config/eniac.php
+++ config/eniac.php
@@ -3,10 +3,13 @@
 use League\Config\Configuration;
 use Nette\Schema\Expect;
 
-// Define your configuration schema
-$config = new Configuration([
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->safeLoad();
+
+// Define your eniacuration schema
+$eniac = new Configuration([
     'database' => Expect::structure([
-        'driver' => Expect::anyOf('mysql', 'postgresql', 'sqlite')->required(),
+        'driver' => Expect::anyOf('pdo_mysql', 'postgresql', 'sqlite')->required(),
         'host' => Expect::string()->default('localhost'),
         'port' => Expect::int()->min(1)->max(65535),
         'database' => Expect::string()->required(),
@@ -14,15 +17,16 @@ $config = new Configuration([
         'password' => Expect::string()->nullable(),
     ]),
     'logging' => Expect::structure([
-        'enabled' => Expect::bool()->default($_ENV['DEBUG'] == true),
+        'enabled' => Expect::bool()->default((bool) $_ENV['DEBUG'] == true),
         'path' => Expect::string()->assert(function ($path) { return \is_writeable($path); })->required(),
+        'file' => Expect::string()->required(),
     ]),
 ]);
 
 // Set the values somewhere
 $userProvidedValues = [
     'database' => [
-        'driver' => 'mysql',
+        'driver' => 'pdo_mysql',
         'port' => 3306,
         'host' => 'localhost',
         'database' => 'getloan_com',
@@ -30,14 +34,17 @@ $userProvidedValues = [
         'password' => 'èóíßù5íWêïË4458é',
     ],
     'logging' => [
-        'path' => __DIR__ . '/log/app.log',
+        'path' => __DIR__ . '/../log',
+        'file' => 'app.log',
+        'enabled' => (bool) $_SERVER['DEBUG'],
     ],
 ];
 
-// Merge those values into your configuration schema:
-$config->merge($userProvidedValues);
+// Merge those values into your eniacuration schema:
+$eniac->merge($userProvidedValues);
 
 // Read the values and do stuff with them
-if ($config->get('logging.enabled')) {
-    file_put_contents($config->get('logging.path'), 'Connecting to the database on ' . $config->get('database.host'));
+if ($eniac->get('logging.enabled')) {
+    file_put_contents($eniac->get('logging.path') . $eniac->get('logging.file'),
+     'Connecting to the database on ' . $eniac->get('database.host'));
 }

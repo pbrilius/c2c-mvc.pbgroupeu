ad5acf1631af95aa9a78da6191c456e941a49a5a
Povilas Brilius <pbrilius@gmail.com>
2021-08-01 22:53:34 +0300

    Doctrine ORM in entity manager interface - first attempt.

---
+++ config/container.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+$container = new League\Container\Container();
+
+require_once __DIR__ . '/../bootstrap.php';
+
+use Doctrine\ORM\EntityManagerInterface;
+
+$container->add(EntityManagerInterface::class,$entityManager)->addTag('doctrine.orm');

549b9fd3de7b8e54cff0720217f409ce97669dc7
Povilas Brilius <pbrilius@gmail.com>
2021-08-02 13:43:18 +0300

    "Hello world!" release.

--- public/index.php
+++ public/index.php
@@ -1,1 +1,25 @@
 <?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+$request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+);
+
+$router = new League\Route\Router;
+
+// map a route
+$router->map('GET', '/', function (ServerRequestInterface $request): ResponseInterface {
+    $response = new Laminas\Diactoros\Response;
+    $response->getBody()->write('<h1>Hello, World!</h1>');
+    return $response;
+});
+
+$response = $router->dispatch($request);
+
+// send the response to the browser
+(new Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
